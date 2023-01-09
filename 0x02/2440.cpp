e6fc7c90a534ea648783afd81ba766bbdbf3505b
eeheueklf <eeheueklf@gmail.com>
2023-01-09 15:15:50 +0900

    {기초코드 2}

---
+++ 0x02/2440.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    for(int i=0;i<N;i++){
+        for(int j=N;j>i;j--)
+            cout << "*";
+        cout << "\n";
+    }
+}
---
+++ 0x02/2443.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<i;j++)
+            cout << " ";
+        for(int j=0;j<(N-i)*2-1;j++)
+            cout << "*";
+        cout << "\n";
+    }
+}
+
+// 4 1
+// 3 3
+// 2 5
+// 1 7
+// 0 9
