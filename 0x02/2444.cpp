9cb39ec389c862bf61478e22f5bb9619cf3eb6e9
eeheueklf <eeheueklf@gmail.com>
2023-01-09 15:27:06 +0900

    기초코드2

---
+++ 0x02/2444.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N-i-1;j++)
+            cout << " ";
+        for(int j=0;j<i*2+1;j++)
+            cout << "*";
+        cout << "\n";
+    }
+    for(int i=N-1;i>0;i--){
+        for(int j=0;j<N-i;j++)
+            cout << " ";
+        for(int j=0;j<2*i-1;j++)
+            cout << "*";
+        cout << "\n";
+    }
+
+}
+
